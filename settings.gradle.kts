7355e9a8d3f1217ef01b8e47f4d3e182d5507715
Niels Marsman <niels.marsman@gmail.com>
2026-03-22 23:02:11 +0100

    SMART-13 - Project setup; Initialized gradle and set it up to start building

---
+++ settings.gradle.kts
@@ -0,0 +1,20 @@
+rootProject.name = "smarthome-api"
+
+enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
+
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+include(":api")
